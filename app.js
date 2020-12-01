941245a1a659ebc5ba3782a228a107c37802c92d
Rim Sungatovich <rsungatovichsocial@gmail.com>
2020-12-01 14:11:25 +0300

    added cors config

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+require('dotenv')
+  .config();
+const cors = require('cors');
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -13,12 +15,26 @@ const centralErrorsHandler = require('./middlewares/centralErrorsHandler');
 
 const app = express();
 
+const options = {
+  origin: [
+    'http://localhost:8080',
+    'https://chirikgaga.ga',
+    'https://rsungatovich.github.io',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
 mongoose.connect(DATABASE_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
 
+app.use(cors(options));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(requestLogger);
